7d4aea540b9c190343f92507920937ee7bc3b1e3
jangyeohoon <someday0307@naver.com>
2023-07-03 23:45:43 +0900

    0630
    0630위클리 과제

---
+++ iOS_072_Jangyeohoon/iOS_072_Jangyeohoon/iOS_072_JangyeohoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_072_JangyeohoonApp.swift
+//  iOS_072_Jangyeohoon
+//
+//  Created by 장여훈 on 2023/07/02.
+//
+
+import SwiftUI
+
+@main
+struct iOS_072_JangyeohoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
