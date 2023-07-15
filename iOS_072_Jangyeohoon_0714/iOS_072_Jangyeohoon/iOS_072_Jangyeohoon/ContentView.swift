ac6cfc1e4c90f3d5ef6dffb3160a780da05c3bce
jangyeohoon <someday0307@naver.com>
2023-07-15 22:49:11 +0900

    0714

---
+++ iOS_072_Jangyeohoon_0714/iOS_072_Jangyeohoon/iOS_072_Jangyeohoon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS_072_Jangyeohoon
+//
+//  Created by 장여훈 on 2023/07/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
