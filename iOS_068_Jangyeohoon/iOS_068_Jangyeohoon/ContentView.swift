ffb37a3e6089ac58d621efc7833d3aa8dea98b23
jangyeohoon <someday0307@naver.com>
2023-07-29 18:44:40 +0900

    0728과제

---
+++ iOS_068_Jangyeohoon/iOS_068_Jangyeohoon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS_068_Jangyeohoon
+//
+//  Created by 장여훈 on 2023/07/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_068_Jangyeohoon/iOS_068_Jangyeohoon/iOS_068_JangyeohoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_068_JangyeohoonApp.swift
+//  iOS_068_Jangyeohoon
+//
+//  Created by 장여훈 on 2023/07/29.
+//
+
+import SwiftUI
+
+@main
+struct iOS_068_JangyeohoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
